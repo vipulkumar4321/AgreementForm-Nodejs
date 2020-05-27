d379e7ae6a623e2cd27dcf6f150576a50a4c43dd
unknown <vipulkumar4321@gmail.com>
2020-05-27 13:59:15 +0530

    Final push

--- app.js
+++ app.js
@@ -25,11 +25,15 @@ mongoose.connect(
 
 //Schema setup
 var vendorSchema = new mongoose.Schema({
+  time: String,
+  venueType: String,
+  ownerName: String,
   name: String,
   address: String,
   email: String,
   contactNumber: String,
   whatsappNumber: String,
+
 });
 
 var Vendor = mongoose.model("Vendor", vendorSchema);
@@ -43,29 +47,34 @@ app.get("/agreement", function (req, res) {
 });
 
 app.post("/post", function (req, res) {
-  var name = req.body.company_Name;
+  var venueType = req.body.venueType;
+  var ownerName = req.body.ownerName;
+  var name = req.body.companyName;
   var address = req.body.address;
-  var email = req.body.emailId;
+  var email = req.body.email;
   var contactNumber = req.body.contactNumber;
   var whatsappNumber = req.body.whatsappNumber;
   var aadhaarFront = req.files.aadhaarFront;
   var aadhaarBack = req.files.aadhaarBack;
   var panFront = req.files.panFront;
-  var panBack = req.files.panBack;
+  var gstCertificate = req.files.gstCertificate;
   var cheque = req.files.cheque;
   var signature = req.files.signature;
   var photo = req.files.photo;
 
   // console.log(req.files);
 
-  time = new Date().getTime().toString();
+  var time = new Date().getTime().toString();
 
-  const uploadFile = upload(time, aadhaarFront, aadhaarBack, panFront, panBack, signature, photo, cheque);
+  const uploadFile = upload(time, aadhaarFront, aadhaarBack, panFront, gstCertificate, signature, photo, cheque);
 
   console.log(uploadFile);
 
-  if(uploadFile.status==true){
+  if (uploadFile.status == true) {
     var newVendor = {
+      time: time,
+      venueType: venueType,
+      ownerName: ownerName,
       name: name,
       address: address,
       email: email,
@@ -76,217 +85,204 @@ app.post("/post", function (req, res) {
       if (err) {
         console.log(err);
       } else {
-        res.send(
-          {
-            "status":true,
-            "msg":uploadFile.msg
-          }
-        )
+        res.send({
+          "status": true,
+          "msg": uploadFile.msg
+        })
       }
     });
-  }
-
-  else{
-    res.send(
-      {
-        "status":false,
-        "msg":uploadFile.msg
-      }
-    )
+  } else {
+    res.send({
+      "status": false,
+      "msg": uploadFile.msg
+    })
   }
 });
 
-function upload(time, aadhaarFront, aadhaarBack, panFront, panBack, signature, photo, cheque){
+function upload(time, aadhaarFront, aadhaarBack, panFront, gstCertificate, signature, photo, cheque) {
 
   const uploadAadhaarFrontFile = uploadAadhaarFront(time, aadhaarFront);
   const uploadAadhaarBackFile = uploadAadhaarBack(time, aadhaarBack);
   const uploadPanFrontFile = uploadPanFront(time, panFront);
-  const uploadPanBackFile = uploadPanBack(time, panBack);
+  const uploadGstCertificateFile = uploadGstCertificate(time, gstCertificate);
   const uploadSignatureFile = uploadSignature(time, signature);
   const uploadPhotoFile = uploadPhoto(time, photo);
   const uploadChequeFile = uploadCheque(time, cheque);
 
-  if(uploadAadhaarFrontFile.status==false){
+  if (uploadAadhaarFrontFile.status == false) {
     return {
-      status:uploadAadhaarFile.status,
-      msg:uploadAadhaarFile.msg
+      status: uploadAadhaarFile.status,
+      msg: uploadAadhaarFile.msg
     }
   }
 
-  if(uploadAadhaarBackFile.status==false){
+  if (uploadAadhaarBackFile.status == false) {
     return {
-      status:uploadAadhaarFile.status,
-      msg:uploadAadhaarFile.msg
+      status: uploadAadhaarFile.status,
+      msg: uploadAadhaarFile.msg
     }
   }
 
-  if(uploadPanFrontFile.status==false){
+  if (uploadPanFrontFile.status == false) {
     return {
-      status:uploadPanFile.status,
-      msg:uploadPanFile.msg
+      status: uploadPanFile.status,
+      msg: uploadPanFile.msg
     }
   }
 
-  if(uploadPanBackFile.status==false){
+  if (uploadGstCertificateFile.status == false) {
     return {
-      status:uploadPanFile.status,
-      msg:uploadPanFile.msg
+      status: uploadGstCertificateFile.status,
+      msg: uploadGstCertificateFile.msg
     }
   }
 
-  if(uploadSignatureFile.status==false){
+  if (uploadSignatureFile.status == false) {
     return {
-      status:uploadSignatureFile.status,
-      msg:uploadSignatureFile.msg
+      status: uploadSignatureFile.status,
+      msg: uploadSignatureFile.msg
     }
   }
 
-  if(uploadPhotoFile.status==false){
+  if (uploadPhotoFile.status == false) {
     return {
-      status:uploadPhotoFile.status,
-      msg:uploadPhotoFile.msg
+      status: uploadPhotoFile.status,
+      msg: uploadPhotoFile.msg
     }
   }
 
-  if(uploadChequeFile.status==false){
+  if (uploadChequeFile.status == false) {
     return {
-      status:uploadChequeFile.status,
-      msg:uploadChequeFile.msg
+      status: uploadChequeFile.status,
+      msg: uploadChequeFile.msg
     }
   }
 
   return {
-    status:true,
-    msg:"All Files Uploaded"
+    status: true,
+    msg: "All Files Uploaded"
   }
 }
 
-function uploadAadhaarFront(time, aadhaarFront){
+function uploadAadhaarFront(time, aadhaarFront) {
   aadhaarFront.mv('./uploaded_files/Aadhaar/front/' + time + "_" + aadhaarFront.name, function (err) {
-    if (err)
-    {
+    if (err) {
       return {
-        "status":false,
-        "msg":err                
-      };
-    }      
-    console.log('Aadhaar Front File Uploaded');
-  
-  });
-      return {
-        "status":true,
-        "msg":"Aadhaar Front File Uploaded"             
-      };
-
-
-}
-
-function uploadAadhaarBack(time, aadhaarBack){
-    aadhaarBack.mv('./uploaded_files/Aadhaar/back/' + time + "_" + aadhaarBack.name, function (err) {
-      if (err)
-    {
-      return {
-        "status":false,
-        "msg":err                
+        "status": false,
+        "msg": err
       };
     }
-  
-      console.log('Aadhaar Back File Uploaded');
-    });
-        return {
-          "status":true,
-          "msg":"Aadhaar Back File Uploaded"             
-        };
+    console.log('Aadhaar Front File Uploaded');
+
+  });
+  return {
+    "status": true,
+    "msg": "Aadhaar Front File Uploaded"
+  };
+
+
 }
 
-function uploadPanFront(time, panFront){
-  panFront.mv('./uploaded_files/PAN/front/' + time + "_" + panFront.name, function (err) {
-    if (err)
-    {
+function uploadAadhaarBack(time, aadhaarBack) {
+  aadhaarBack.mv('./uploaded_files/Aadhaar/back/' + time + "_" + aadhaarBack.name, function (err) {
+    if (err) {
       return {
-        "status":false,
-        "msg":err                
+        "status": false,
+        "msg": err
+      };
+    }
+
+    console.log('Aadhaar Back File Uploaded');
+  });
+  return {
+    "status": true,
+    "msg": "Aadhaar Back File Uploaded"
+  };
+}
+
+function uploadPanFront(time, panFront) {
+  panFront.mv('./uploaded_files/PAN/front/' + time + "_" + panFront.name, function (err) {
+    if (err) {
+      return {
+        "status": false,
+        "msg": err
       };
     }
 
     console.log('PAN Front File Uploaded');
   });
-      return {
-        "status":true,
-        "msg":'PAN Front File Uploaded'
-      };
+  return {
+    "status": true,
+    "msg": 'PAN Front File Uploaded'
+  };
 }
 
-function uploadPanBack(time, panBack){
-    panBack.mv('./uploaded_files/PAN/back/' + time + "_" + panBack.name, function (err) {
-      if (err)
-    {
+function uploadGstCertificate(time, gstCertificate) {
+  gstCertificate.mv('./uploaded_files/GST_certificate/' + time + "_" + gstCertificate.name, function (err) {
+    if (err) {
       return {
-        "status":false,
-        "msg":err                
+        "status": false,
+        "msg": err
       };
     }
-  
-      console.log('PAN Back File Uploaded');
-    });
-        return {
-          "status":true,
-          "msg":'PAN Back File Uploaded'
-        };
+
+    console.log('GST Certificate File Uploaded');
+  });
+  return {
+    "status": true,
+    "msg": 'GST Certificate File Uploaded'
+  };
 }
 
-function uploadSignature(time, signature){
+function uploadSignature(time, signature) {
   signature.mv('./uploaded_files/signature/' + time + "_" + signature.name, function (err) {
-    if (err)
-    {
+    if (err) {
       return {
-        "status":false,
-        "msg":err                
+        "status": false,
+        "msg": err
       };
     }
 
     console.log('Signature File uploaded!');
   });
-      return {
-        "status":true,
-        "msg":'Signature File uploaded!'
-      };
+  return {
+    "status": true,
+    "msg": 'Signature File uploaded!'
+  };
 }
 
-function uploadPhoto(time, photo){
+function uploadPhoto(time, photo) {
   photo.mv('./uploaded_files/Photo/' + time + "_" + photo.name, function (err) {
-    if (err)
-    {
+    if (err) {
       return {
-        "status":false,
-        "msg":err                
+        "status": false,
+        "msg": err
       };
     }
 
     console.log('Photo File uploaded!');
   });
-      return {
-        "status":true,
-        "msg":'Photo File uploaded!'
-      };
+  return {
+    "status": true,
+    "msg": 'Photo File uploaded!'
+  };
 }
 
-function uploadCheque(time, cheque){
+function uploadCheque(time, cheque) {
   cheque.mv('./uploaded_files/Cheque/' + time + "_" + cheque.name, function (err) {
-    if (err)
-    {
+    if (err) {
       return {
-        "status":false,
-        "msg":err                
+        "status": false,
+        "msg": err
       };
     }
 
     console.log('Cheque File uploaded!');
   });
-      return {
-        "status":true,
-        "msg":'Cheque File uploaded!'                
-      };
+  return {
+    "status": true,
+    "msg": 'Cheque File uploaded!'
+  };
 }
 
 app.get("/step1", function (req, res) {
--- public/step1.js
+++ public/step1.js
@@ -1,50 +1,84 @@
 function sub() {
     var form = new FormData();
 
+    const venueType = $("#inputTypeOfVenue").val();
+    const ownerName = $("#inputOwnerName").val();
     const companyName = $("#composedName").val();
     const address = $("#inputAddress").val();
-    const emailId = $("#inputEmail").val();
+    const email = $("#inputEmail").val();
     const contactNumber = $("#contactNumber").val();
     const whatsappNumber = $("#whatsappNumber").val();
+
     const aadhaarFront = $("#aadhaarFront")[0].files[0];
     const aadhaarBack = $("#aadhaarBack")[0].files[0];
     const panFront = $("#panFront")[0].files[0];
-    const panBack = $("#panBack")[0].files[0];
+    const gstCertificate = $("#gstCertificate")[0].files[0];
     const cheque = $("#cheque")[0].files[0];
     const signature = $("#signature")[0].files[0];
     const photo = $("#photo")[0].files[0];
 
+
+    if (venueType === "") {
+        alert("Venue Type can't be blank !");
+        return 0;
+    }
+    if (ownerName === "") {
+        alert("Owner name can't be blank !");
+        return 0;
+    }
+    if (companyName === "") {
+        alert("Company name can't be blank !");
+        return 0;
+    }
+    if (address === "") {
+        alert("Address can't be blank !");
+        return 0;
+    }
+    if (email === "") {
+        alert("Email can't be blank !");
+        return 0;
+    }
+    if (contactNumber === "") {
+        alert("Contact number can't be blank !");
+        return 0;
+    }
+    if (whatsappNumber === "") {
+        alert("Whatsapp number can't be blank !");
+        return 0;
+    }
+
+    form.append("venueType", venueType);
+    form.append("ownerName", ownerName);
     form.append("companyName", companyName);
     form.append("address", address);
-    form.append("email", emailId);
+    form.append("email", email);
     form.append("contactNumber", contactNumber);
     form.append("whatsappNumber", whatsappNumber);
     form.append("aadhaarFront", aadhaarFront);
     form.append("aadhaarBack", aadhaarBack);
     form.append("panFront", panFront);
-    form.append("panBack", panBack);
+    form.append("gstCertificate", gstCertificate);
     form.append("cheque", cheque);
     form.append("signature", signature);
     form.append("photo", photo);
 
     var settings = {
-        "url": "/post",
-        "method": "POST",
-        "timeout": 0,
-        "processData": false,
-        "mimeType": "multipart/form-data",
-        "contentType": false,
-        "data": form
+        url: "/post",
+        method: "POST",
+        timeout: 0,
+        processData: false,
+        mimeType: "multipart/form-data",
+        contentType: false,
+        data: form,
     };
-    
+
     $.ajax(settings).done(function (response) {
         response = JSON.parse(response);
         console.log(typeof response);
-        if(response.status == true){
+        if (response.status == true) {
             console.log(response.msg);
             window.location = "/thanks";
-        }
-        else{
+        } else {
             console.log(response.msg);
         }
     });
